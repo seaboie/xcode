d4aa33b4adb3cb7d5944b6e586cad0291d0f28c5
kritbovorn <kritbovorn@gmail.com>
2023-06-12 12:39:34 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by krit on 12/6/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
