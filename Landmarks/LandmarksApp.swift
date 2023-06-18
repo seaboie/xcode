a8585052525f33fcb598cbb531c239090bdd2dde
kritbovorn <kritbovorn@gmail.com>
2023-06-18 19:40:45 +0700

    Adding TabView and tabItem()

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -14,7 +14,7 @@ struct LandmarksApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
                 .environmentObject(order)
         }
     }
