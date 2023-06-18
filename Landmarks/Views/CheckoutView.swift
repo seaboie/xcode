f052ca602e5714654041e42957cdddf1816fc5a6
kritbovorn <kritbovorn@gmail.com>
2023-06-18 21:46:34 +0700

    Two-way binding in SwiftUI

--- Landmarks/Views/CheckoutView.swift
+++ Landmarks/Views/CheckoutView.swift
@@ -11,18 +11,39 @@ struct CheckoutView: View {
     
     @EnvironmentObject var order: Order;
     @State private var paymentType = "Cash";
-    
+    @State private var isAddLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    let tipAmounts = [10, 15, 20, 25, 0]
+    @State private var tipAmount = 15;
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"];
     
     var body: some View {
-        VStack {
+        Form {
             Section {
                 Picker("How do you want to pay ?", selection: $paymentType) {
                     ForEach(paymentTypes, id: \.self) { item in
                         Text(item)
                     }
                 }
+                Toggle("Add iDine loyalty card", isOn: $isAddLoyaltyDetails.animation())
+                
+                if (isAddLoyaltyDetails) {
+                    TextField("Enter your iDine ID", text: $loyaltyNumber, axis: .horizontal)
+                }
             }
+            Section("Add a Tip", content: {
+                Picker("Percentage: ", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) { amount in
+                        Text("\(amount)%")
+                    }
+                }
+                .pickerStyle(.segmented)
+            })
+            Section("Total: $100", content: {
+                Button("Confirm Order") {
+                    //
+                }
+            })
             .navigationTitle("Payment")
             .navigationBarTitleDisplayMode(.inline)
         }
--- Landmarks/Views/OrderView.swift
+++ Landmarks/Views/OrderView.swift
@@ -26,7 +26,7 @@ struct OrderView: View {
                 
                 Section {
                     NavigationLink("Place Order") {
-                        Text("Check Out")
+                        CheckoutView()
                     }
                 }
             }
